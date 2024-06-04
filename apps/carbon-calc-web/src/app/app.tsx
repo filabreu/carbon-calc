41e356d34f80d506790c5bfd8a664630952709d8
Filipe Abreu <filabreu@gmail.com>
2024-06-03 21:33:33 -0300

    Add basic routes for holding the different pages of the app

--- apps/carbon-calc-web/src/app/app.tsx
+++ apps/carbon-calc-web/src/app/app.tsx
@@ -1,6 +1,51 @@
+import { Route, Routes, Link } from 'react-router-dom';
+
 export function App() {
   return (
-    <div></div>
+    <div>
+
+      {/* START: routes */}
+      <div role="navigation">
+        <ul>
+          <li>
+            <Link to="/home-energy">Home Energy</Link>
+          </li>
+          <li>
+            <Link to="/transportation">Transportation</Link>
+          </li>
+          <li>
+            <Link to="/waste">Waste</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route
+          path="/home-energy"
+          element={
+            <div>
+              Home Energy
+            </div>
+          }
+        />
+        <Route
+          path="/transportation"
+          element={
+            <div>
+              Transportation
+            </div>
+          }
+        />
+        <Route
+          path="/waste"
+          element={
+            <div>
+              Waste
+            </div>
+          }
+        />
+      </Routes>
+      {/* END: routes */}
+    </div>
   );
 }
 
