392f6b4b3872fe9ca14fe0e9f9d39bd15fad5b00
Filipe Abreu <filabreu@gmail.com>
2024-06-03 21:22:49 -0300

    Generate carbon-calc-web React application

---
+++ apps/carbon-calc-web-e2e/cypress.config.ts
@@ -0,0 +1,17 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'nx run carbon-calc-web:serve',
+        production: 'nx run carbon-calc-web:preview',
+      },
+      ciWebServerCommand: 'nx run carbon-calc-web:serve-static',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
